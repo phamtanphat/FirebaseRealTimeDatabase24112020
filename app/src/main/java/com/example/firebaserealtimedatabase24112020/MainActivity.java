1f6a22daea27483bad686009f0a070f8d22037d5
phatdroid94@gmail.com <phatdroid94@gmail.com>
2021-03-30 18:08:55 +0700

    doc du lieu singlevalueevent

--- app/src/main/java/com/example/firebaserealtimedatabase24112020/MainActivity.java
+++ app/src/main/java/com/example/firebaserealtimedatabase24112020/MainActivity.java
@@ -4,12 +4,20 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -39,17 +47,17 @@ public class MainActivity extends AppCompatActivity {
         // Phuongtien
             // ten : xe dap
             // sobanh : 2 bánh
-        Phuongtien xedap = new Phuongtien("xe đạp" , 2);
-        myRef.child("Phuongtien").setValue(xedap).addOnCompleteListener(new OnCompleteListener<Void>() {
-            @Override
-            public void onComplete(@NonNull Task<Void> task) {
-                if (task.isSuccessful()){
-                    Toast.makeText(MainActivity.this, "Thêm dữ liệu thành công", Toast.LENGTH_SHORT).show();
-                }else{
-                    Toast.makeText(MainActivity.this, task.getException().getMessage(), Toast.LENGTH_SHORT).show();
-                }
-            }
-        });
+//        Phuongtien xedap = new Phuongtien("xe đạp" , 2);
+//        myRef.child("Phuongtien").setValue(xedap).addOnCompleteListener(new OnCompleteListener<Void>() {
+//            @Override
+//            public void onComplete(@NonNull Task<Void> task) {
+//                if (task.isSuccessful()){
+//                    Toast.makeText(MainActivity.this, "Thêm dữ liệu thành công", Toast.LENGTH_SHORT).show();
+//                }else{
+//                    Toast.makeText(MainActivity.this, task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+//                }
+//            }
+//        });
 
         // 3 : Kiểu list object
 
@@ -71,6 +79,40 @@ public class MainActivity extends AppCompatActivity {
 //                        }
 //                    }
 //                });
+
+        // Đọc dữ liệu
+//        myRef.child("Android24112020").addValueEventListener(new ValueEventListener() {
+//            @Override
+//            public void onDataChange(@NonNull DataSnapshot snapshot) {
+//                String value = (String) snapshot.getValue();
+//                Toast.makeText(MainActivity.this, value , Toast.LENGTH_SHORT).show();
+//            }
+//
+//            @Override
+//            public void onCancelled(@NonNull DatabaseError error) {
+//
+//            }
+//        });
+
+        myRef.child("Phuongtien").addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                HashMap<String,String> hashMap = (HashMap<String, String>) snapshot.getValue();
+                Iterator iterator = hashMap.entrySet().iterator();
+
+                while (iterator.hasNext()){
+                    Map.Entry entry = (Map.Entry) iterator.next();
+                    Log.d("BBB",entry.getKey().toString());
+                    Log.d("BBB",entry.getValue().toString());
+                    iterator.remove();
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
 
     }
 }
